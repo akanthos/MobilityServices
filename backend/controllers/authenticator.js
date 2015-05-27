69549d08d6a29a90c0153869f6e4d60e832c2986
fr33g <glaser.ig@gmail.com>
2015-05-27 11:15:53 +0200

    Allow CORS

--- backend/controllers/authenticator.js
+++ backend/controllers/authenticator.js
@@ -5,6 +5,9 @@ module.exports = function (context) {
 
 	return {
 		authMiddleware : function (req, res, next) {
+			res.header('Access-Control-Allow-Origin', '*');
+    		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    		res.header('Access-Control-Allow-Headers', 'Content-Type');
 			if(req.url == '/login') {
 				userCtrl.login(req.body.email, req.body.password).then(function(token) {
 					return res.status(200).send({token: token});
